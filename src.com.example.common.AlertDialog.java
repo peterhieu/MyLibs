e161c00680ad4af5211ceb702242b2eb03223f3e
peterhieu <mt.hieucse09@gmail.com>
2013-08-20 14:50:06 +0700

    Create src.com.example.common.AlertDialog.java

---
+++ src.com.example.common.AlertDialog.java
@@ -0,0 +1,15 @@
+package com.example.common;
+
+
+import android.widget.Button;
+import android.widget.TextView;
+
+public class AlertDialog{
+	public TextView m_msgTitleTextView;
+	public TextView m_msgContentTextView;
+	public Button m_btnConfirm;
+	private void AlertDialog(String msgTitle, String msgContent){}
+	private void Show(){
+		
+	}
+}
