4d6f397b8c6e873773e946de403b132dec3f8221
peterhieu <mt.hieucse09@gmail.com>
2013-08-20 14:51:18 +0700

    Create src.com.example.common.DisplayActivity.java

---
+++ src.com.example.common.DisplayActivity.java
@@ -0,0 +1,12 @@
+package com.example.common;
+
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class DisplayActivity {
+	public TextView m_SystemTimeTextView;
+	public ListView m_SystemTimeListView;
+	public Button m_CancelButton;
+	private void btnCancel_OnClick(){}
+}
