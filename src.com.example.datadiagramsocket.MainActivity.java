8a1bbc8a9027b6130ac0ab607cac2ddb41e24e2f
peterhieu <mt.hieucse09@gmail.com>
2013-08-26 17:21:27 +0700

    Update src.com.example.datadiagramsocket.MainActivity.java

--- src.com.example.datadiagramsocket.MainActivity.java
+++ src.com.example.datadiagramsocket.MainActivity.java
@@ -1,29 +1,65 @@
 package com.example.datadiagramsocket;
 
+import com.example.common.AlertDialog;
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends Activity {
+	
 	public EditText m_IPAddressEditText;
 	public EditText m_PortEditText;
-	public Button m_ReceiveButton;
-	public Button m_QuitButton;
+	public Button 	m_ReceiveButton;
+	public Button 	m_QuitButton;
+
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.settings_screen);
+		
+		m_IPAddressEditText=(EditText)findViewById(R.id.edtIPAddress);
+		m_PortEditText=(EditText)findViewById(R.id.edtPort);
 	}
+	
+	public void btnReceive_OnClick(View view){
+		
+		String ip 	= m_IPAddressEditText.getText().toString();
+		String port = m_PortEditText.getText().toString();
+		
+		if (ip.equals("")) {
+			AlertDialog dialog= new AlertDialog (this,"Error","No ip address");
+			dialog.Show();
+		}
+		else if (port.equals("")) {
+			AlertDialog dialog= new AlertDialog (this,"Error","No data port");
+			dialog.Show();
+		}
+		else {	
+			
+			Bundle bndl = new Bundle();
+			bndl.putCharSequence("ip", ip);
+			bndl.putCharSequence("port", port);
+			
+//			Intent i= new Intent(MainActivity.this,DisplayActivity.class);	
+//			i.putExtra("intentFromMain", bndl);			
+//			startActivity(i);
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+			Intent i2= new Intent(MainActivity.this,DisplayTextRecievedActivity.class);	
+			i2.putExtra("intentFromMain", bndl);			
+			startActivity(i2);
+			
+			finish();
+		}		
+		
 	}
-	private void btnReceive_OnClick(){}
-	private void btnQuit_OnClick(){}
+	
+	public void btnQuit_OnClick(View view){
+		finish();
+	}
+	
 
 }
