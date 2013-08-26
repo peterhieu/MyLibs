b6040f9858e9342c31ec63b8f56b7b282e2a1089
peterhieu <mt.hieucse09@gmail.com>
2013-08-26 17:22:10 +0700

    Update src.com.example.datadiagramsocket.DataDiagramSocket.java

--- src.com.example.datadiagramsocket.DataDiagramSocket.java
+++ src.com.example.datadiagramsocket.DataDiagramSocket.java
@@ -7,42 +7,53 @@ import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 
-import android.content.Intent;
 import android.util.Log;
 
-
-public class DataDiagramSocket {
+public class DataDiagramSocket implements Runnable{
 	
-	private static DataDiagramSocket m_instance = null;	//Singleton instance of DataDiagramSocket
-	private static int m_defPort;					//Port to connect UDP
-	private static InetAddress m_defIPAddress;		//IP Address to connect UDP
-	private DatagramSocket m_datagramSocket;		//Used to receive System Time data from Windows Simulation via UDP
-	private DatagramPacket m_datagramPacket;		//Used to store system time package
+	private static DataDiagramSocket 	m_instance;
+	private static int 					m_defPort;
+	private static InetAddress			m_defIPAddress;
+	private DatagramSocket 				m_datagramSocket;
+	private DatagramPacket 				m_datagramPacket;
 	
+	private DataDiagramSocket()throws SocketException, UnknownHostException {
+		
+		setPort(Integer.parseInt(DisplayTextRecievedActivity.port));
+		setBindAddress(InetAddress.getByName(DisplayTextRecievedActivity.ip));
+		
+	}
 	
-	private DataDiagramSocket() {}
-	
-	public final static synchronized DataDiagramSocket getInstance() {
-		//data out: DataDiagramSocket Singleton 		
+	public static DataDiagramSocket getInstance() {
+		
 		if(m_instance == null) {
-			m_instance = new DataDiagramSocket();
+			try {				
+				m_instance = new DataDiagramSocket();		
+			} catch (SocketException e) {
+				// TODO Auto-generated catch block
+				Log.d("m_instance = new DataDiagramSocket", "exception");
+				e.printStackTrace();
+			} catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				Log.d("m_instance = new DataDiagramSocket", "exception");
+				e.printStackTrace();
+			}
 		}	
+		
 		return m_instance;
 	 }
 	
 	public final static void setPort(int port) {
 		// data in : Port value to connect UDP
-		m_defPort=port;
+		m_defPort = port;
 	}
 	
 	public final static void setBindAddress(InetAddress ipAddr) {
 		// data in : IP Address value to connect UDP
-		m_defIPAddress=ipAddr;
+		m_defIPAddress = ipAddr;
 	}
 	
-	
-
-	public final int receiveDatagram(DatagramPacket dgram){
+	public final int receiveDatagram(DatagramPacket dgram) {
 		//data in: DatagramPacket to receive with length of buffer
 		//data out : return Length of buffer which be received
 		return dgram.getLength();
@@ -50,49 +61,37 @@ public class DataDiagramSocket {
 	
 	public static String stringFromPacket(DatagramPacket packet) {
 		//data in  : DatagramPacket for converting to string   
-		// return data out : String result from DatagramPacket
-		 return new String(packet.getData(), 0, packet.getLength());
+		//return data out : String result from DatagramPacket
+		return new String(packet.getData(), 0, packet.getLength());
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		while(true){
+			try {	
+				
+				byte[] 	message 		= new byte[127];
+				m_datagramPacket 		= new DatagramPacket(message, message.length); 
+				m_datagramSocket 		= new DatagramSocket(m_defPort, m_defIPAddress);
+				
+				m_datagramSocket.setSoTimeout(10000);			
+				m_datagramSocket.receive(m_datagramPacket);
+				
+				DisplayTextRecievedActivity.handerThr.post(new DisplayTextRecievedActivity.UpdateThread(m_datagramPacket.getData()));				
+				m_datagramSocket.close();
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			} 		    	
+		}	
 	}
 	
-	public static void processUDP() {
-		String textReceivedFromServer;
-		
-	    
-	    try {
-	    			    	
-	    	byte[] message = new byte[50];
-	    	
-	    	InetAddress inetServer = InetAddress.getByName("10.0.2.15");
-		
-	    	DatagramPacket p = new DatagramPacket(message, message.length); 
-
-			DatagramSocket s = new DatagramSocket(5000, inetServer);
-
-			s.setSoTimeout(20000);
-			
-			s.receive(p); 
-			
-			textReceivedFromServer = new String(message, 0, p.getLength());
-			
-		    Log.d("Udp tutorial","message:" + textReceivedFromServer);		
-
-		    //**********************************************************************	
-		    
-/*			Intent i= new Intent(SettingUDP.this, DisplayTextRecievedActivity.class);	
-			i.putExtra("textReceived", textReceivedFromServer);
-			startActivity(i);
-*/			
-			s.close();
-			 
-		} catch (SocketException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	public InetAddress get_ip() {
+		return m_defIPAddress;
+	}
 	
-	}		
-	
-
+	public int get_port() {
+		return m_defPort;
+	}
 }
